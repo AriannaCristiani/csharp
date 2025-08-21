fc2e3f158d465d3f6b61d30a6aeeaac082f3f954
AriannaCrisriani <ariannacristiani@yahoo.it>
2025-08-21 14:25:35 +0200

    Iterazioni

---
+++ asterischi/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Asterischi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int righe;
+
+            while (true)
+            {
+                Console.Write("indica un numero ('no' per uscire): ");
+                string s = Console.ReadLine();
+
+                if (s.ToLower() == "no")
+                {
+                    Console.WriteLine("Arrivederci!");
+                    break;
+                }
+
+                righe = Convert.ToInt32(s);
+
+                for (int r = 0; r <= righe; r++)
+                {
+                    for (int ast = 1; ast <= r; ast++)
+                    {
+                        Console.Write("* ");
+                    }
+                    Console.WriteLine("\n");
+                }
+            }
+        }
+    }
+}
