f7f673ff7cbc76f307d62ee0e224cd20a2ab106e
AriannaCrisriani <ariannacristiani@yahoo.it>
2025-08-27 15:21:47 +0200

    esercizio interface

--- biblioteca2/Program.cs
+++ biblioteca2/Program.cs
@@ -1,2 +1,68 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace biblioteca2
+{
+    public interface IUtente
+    {
+        string Id { get; set; }
+        int AnnoIscrizione { get; set; }
+        string Denominazione { get; }
+    }
+
+    public class Persona : IUtente
+    {
+        public string Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Cognome { get; set; }
+        public int AnnoIscrizione { get; set; }
+        public string Denominazione
+        {
+            get { return Id + " " + Nome + " " + Cognome; }
+        }
+    }
+
+
+    public class Organizzazione : IUtente
+    {
+        public string RagioneSociale { get; set; }
+        public string Id { get; set; }
+        public int AnnoIscrizione { get; set; }
+        public string Denominazione
+        {
+            get { return Id + " " + RagioneSociale; }
+        }
+    }
+
+    public class Biblioteca
+    {
+        static IUtente[] utenti;
+
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            persona.Id = "0001";
+            persona.Nome = "Mario";
+            persona.Cognome = "Rossi";
+            persona.AnnoIscrizione = 2016;
+
+            Organizzazione org = new Organizzazione();
+            org.Id = "0002";
+            org.RagioneSociale = "Grandi Lettori SRL";
+            org.AnnoIscrizione = 2010;
+
+            utenti = new IUtente[] { persona, org };
+
+            stampaUtenti();
+        }
+
+        static void stampaUtenti()
+        {
+            foreach (IUtente ute in utenti)
+            {
+                Console.WriteLine(ute.Denominazione);
+            }
+        }
+    }
+}
